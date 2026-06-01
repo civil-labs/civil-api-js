60ac4fbb63de2c9dc9130dab2f0305a3b261e26a
Jackson Arnold <jackson.n.arnold@gmail.com>
2026-06-01 09:39:30 -0400

    feat: update entrypoint for public path

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
-export * from "./civil/parcels/v1/parcels_pb.d.ts";
-export * from "./civil/tiles/v1/tiles_pb.d.ts";
+export * from "./civil/public/parcels/v1/parcels_pb.d.ts";
+export * from "./civil/public/tiles/v1/tiles_pb.d.ts";
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export * from "./civil/parcels/v1/parcels_pb.js";
-export * from "./civil/tiles/v1/tiles_pb.js";
+export * from "./civil/public/parcels/v1/parcels_pb.js";
+export * from "./civil/public/tiles/v1/tiles_pb.js";
