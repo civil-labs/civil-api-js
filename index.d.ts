90d92093ea2bb3d224ce1f1ae37ad76185c302f8
Jackson Arnold <jackson.n.arnold@gmail.com>
2026-06-02 08:57:28 -0400

    feat: add dex service to entrypoint

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
 export * from "./civil/public/parcels/v1/parcels_pb.d.ts";
 export * from "./civil/public/tiles/v1/tiles_pb.d.ts";
+export * from "./civil/public/dex/v1/dex_pb.d.ts"
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export * from "./civil/public/parcels/v1/parcels_pb.js";
 export * from "./civil/public/tiles/v1/tiles_pb.js";
+export * from "./civil/public/dex/v1/dex_pb.js"
