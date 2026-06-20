9c70f2a75d4372d35a6f564bb8754a3c51699df2
Jackson Arnold <jackson.n.arnold@gmail.com>
2026-06-20 17:03:48 -0400

    feat: update index exports

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
 export * from "./civil/public/parcels/v1/parcels_pb.d.ts";
 export * from "./civil/public/tiles/v1/tiles_pb.d.ts";
 export * from "./civil/public/dex/v1/dex_pb.d.ts"
+export * from "./civil/public/sales/v1/sales_pb.d.ts"
+export * from "./civil/public/instance/v1/instance_pb.d.ts"
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 export * from "./civil/public/parcels/v1/parcels_pb.js";
 export * from "./civil/public/tiles/v1/tiles_pb.js";
 export * from "./civil/public/dex/v1/dex_pb.js"
+export * from "./civil/public/sales/v1/sales_pb.js"
+export * from "./civil/public/instance/v1/instance_pb.js"
