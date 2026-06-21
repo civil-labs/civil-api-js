96c48096dfec1c71be3e558e9b0e3818b1f14005
Jackson Arnold <jackson.n.arnold@gmail.com>
2026-06-20 21:32:47 -0400

    feat: add new api exports

--- index.d.ts
+++ index.d.ts
@@ -3,3 +3,6 @@ export * from "./civil/public/tiles/v1/tiles_pb.d.ts";
 export * from "./civil/public/dex/v1/dex_pb.d.ts"
 export * from "./civil/public/sales/v1/sales_pb.d.ts"
 export * from "./civil/public/instance/v1/instance_pb.d.ts"
+export * from "./civil/public/zoning/v1/zoning_pb.d.ts"
+export * from "./civil/public/landuses/v1/land_uses_pb.d.ts"
+export * from "./civil/public/improvements/v1/improvements_pb.d.ts"
--- index.js
+++ index.js
@@ -3,3 +3,6 @@ export * from "./civil/public/tiles/v1/tiles_pb.js";
 export * from "./civil/public/dex/v1/dex_pb.js"
 export * from "./civil/public/sales/v1/sales_pb.js"
 export * from "./civil/public/instance/v1/instance_pb.js"
+export * from "./civil/public/zoning/v1/zoning_pb.js"
+export * from "./civil/public/landuses/v1/land_uses_pb.js"
+export * from "./civil/public/improvements/v1/improvements_pb.js"
